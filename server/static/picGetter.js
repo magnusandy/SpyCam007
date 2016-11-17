09e02c9df92d070ad8c317f3d56dd37cd440ae29
Andrew Magnus <magnusandy@gmail.com>
2016-11-17 12:48:45 -0600

    working site, pulls latest days images

--- server/static/picGetter.js
+++ server/static/picGetter.js
@@ -1,6 +1,19 @@
+function doCallSetCurrentDates()
+{
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
+var today = year + "-" + month + "-" + day;
+var tomorrow = year + "-" + month + "-" + (day+1);
+document.getElementById("firstDate").value = today;
+document.getElementById("secondDate").value = tomorrow;
+doCall();
+}
+
 function doCall() {
-  startTime = (document.getElementById("firstDate").valueAsNumber)/1000 //divide by 1000 to convert from miliseconds to seconds
-  endTime = (document.getElementById("secondDate").valueAsNumber)/1000
+  startTime = ((document.getElementById("firstDate").valueAsNumber)/1000)+21600 //divide by 1000 to convert from miliseconds to seconds then add 21600 which is 6 hours in seconds to align unix time with local time
+  endTime = ((document.getElementById("secondDate").valueAsNumber)/1000)+21600
     var query = "?startTime=" + startTime + "&endTime=" + endTime;
     $.ajax({
         url: "/pictures/" + query,
@@ -17,6 +30,16 @@ function doCall() {
                 pic.title = results[index].Timestamp.substring(0,10);
                 linksDIV.appendChild(pic);
             }
+            //gallery code
+            initializeGallery();
+            document.getElementById('links').onclick = function (event) {
+                event = event || window.event;
+                var target = event.target || event.srcElement,
+                    link = target.src ? target.parentNode : target,
+                    options = {index: link, event: event},
+                    links = this.getElementsByTagName('a');
+                blueimp.Gallery(links, options);
+              };
             blueimp.Gallery(
               document.getElementById('links').getElementsByTagName('a'),
               {
